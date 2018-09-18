345f89ba01ef88513a6bd364843054774dccfde9
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-18 17:02:49 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,16 +46,6 @@ def monopoly_with_fourth_tier
 	monopoly = {railroads:
 		{
 			pieces: 4,
-			names: {},
-			rent_in_dollars: {}
-		}
-}
-end
-
-def monopoly_with_third_tier
-	monopoly = {railroads:
-		{
-			pieces: 4,
 			names: {
 				reading_railroad: {"mortgage_value" => "$100"},
 				pennsylvania_railroad: {"mortgage_value" => "$200"},
@@ -72,3 +62,5 @@ def monopoly_with_third_tier
 	}
 
 end
+
+
